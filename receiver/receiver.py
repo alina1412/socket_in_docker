bdff72d8346ec86a252aa290ecabbc4918e671b3
alina1412 <alina1412@mail.ru>
2022-12-21 17:55:35 +0300

    draft of 2 services

---
+++ receiver/receiver.py
@@ -0,0 +1,11 @@
+import asyncio
+import socket
+
+
+
+async def main():
+    print("Server receiver")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ sender/sender.py
@@ -0,0 +1,11 @@
+import asyncio
+import socket
+
+
+
+async def main():
+    print("Client sender")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
